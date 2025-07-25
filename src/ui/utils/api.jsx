8c9e344d6e5feafcaa3ebfe4484133f4552510e3
Agmir Hassane <hagmir6@gmail.com>
2025-07-25 17:23:48 +0100

    Update api path

--- src/ui/utils/api.jsx
+++ src/ui/utils/api.jsx
@@ -14,7 +14,7 @@ if (import.meta.env.MODE === 'development') {
   baseURL = 'http://localhost:8000/api/';
 
 } else {
-  baseURL = 'http://192.168.1.113/api/';
+  baseURL = 'http://192.168.1.165/api/';
   // baseURL = 'https://agmir.pdfdrive.me/api/'
 }
 
